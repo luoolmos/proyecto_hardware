89c820eb042aeceef59bd0030e27919edc568c41
luoolmos <146450799+luoolmos@users.noreply.github.com>
2024-04-26 16:27:53 +0200

    Revert "Update"
    This reverts commit c4ea39c3a87ec64e9421df614da114c4fb080533.

--- keil_reversi/gpio.d
+++
@@ -1,2 +0,0 @@
-.\gpio.o: gpio.c
-.\gpio.o: C:\Keil_v5\ARM\INC\Philips\LPC210X.H
--- keil_reversi/timer0.h
+++
@@ -1,4 +0,0 @@
-void timer0_init(int interrump, int resetValue);
-unsigned int timer0_read_int_count(void);
-void timer0_starts (void);
-void timer0_end (void);
